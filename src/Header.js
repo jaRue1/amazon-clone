b037dc00344afe1a2d76291259e4e3eccaa93004
jaRue1 <jaruejohnson@outlook.com>
2021-08-15 19:45:47 -0400

    added the stying for the header

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,18 @@
 import React from "react"
 import "./Header.css"
 import SearchIcon from "@material-ui/icons/Search"
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket"
 function Header() {
   return (
     <div className="header">
-      <img className='header__logo'
-      src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
       />
 
       <div className="header__search">
-        <input
-          className="header__searchInput"
-          text="text"
-          />
-          <SearchIcon className='header__searchIcon'/>
+        <input className="header__searchInput" text="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
@@ -28,6 +27,10 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasket />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
