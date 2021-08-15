744aac80157e040fe98b768b61ffaf0eb4c01521
jaRue1 <jaruejohnson@outlook.com>
2021-08-15 19:06:23 -0400

    project setup + header creation

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBcv_Ppl7E38ufKD8DRDronE6jPpH3HCT8",
-  authDomain: "clone-jj.firebaseapp.com",
-  projectId: "clone-jj",
-  storageBucket: "clone-jj.appspot.com",
-  messagingSenderId: "180039189634",
-  appId: "1:180039189634:web:ac2cb2889264a828f0a09b"
-};
