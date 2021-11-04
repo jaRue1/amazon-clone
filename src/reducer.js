a09e45ac17fb0cf0b4a1a89252a3a375c55d8ff7
jaRue1 <jaruejohnson@outlook.com>
2021-11-04 19:46:48 -0400

    completed login

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,6 @@
 export const initialState = {
   basket: [],
+  user: null
 }
 //Selector
 export const getBasketTotal = (basket) => basket.reduce((amount, item) => item.price + amount, 0);
@@ -24,6 +25,11 @@ const reducer = (state, action) => {
         }
         return{
           ...state, basket: newBasket
+        }
+      case "SET_USER":
+        return{
+          ...state,
+          user: action.user,
         }
     default:
       return state;
