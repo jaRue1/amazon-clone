082790ba5ea851cc75358987b1d4a2ea7c8eea98
jaRue1 <jaruejohnson@outlook.com>
2021-11-07 18:38:14 -0500

    completed backend and built orders page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Home from "./Home"
 import Login from "./Login"
 import Checkout from "./Checkout"
 import Payment from "./Payment"
+import Orders from "./Orders"
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
 import { auth } from "./firebase"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { useStateValue } from "./StateProvider"
+
+const promise = loadStripe('pk_test_51Jsxp1H18DnKo1T79riY9zg5FVYcKR1fBRjQjUf7TI6gNKW5MdzGJ4fdw4P8G9X7p0TtIu63gidZbFp02B1aI93o00yX2gLtO0') // public key
+
 function App() {
   const [{}, dispatch] = useStateValue()
   useEffect(() => {
@@ -35,6 +41,11 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+        <Route exact path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+
           <Route exact path="/login">
             <Login />
           </Route>
@@ -46,7 +57,9 @@ function App() {
 
           <Route exact path="/payment">
             <Header />
-            <Payment/>
+            <Elements stripe={promise}>
+               <Payment/>
+            </Elements>
           </Route>
 
           <Route path="/">
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,30 +1,36 @@
-import React from 'react'
-import './CheckoutProduct.css'
+import React from "react"
+import "./CheckoutProduct.css"
 import { useStateValue } from "./StateProvider"
 
-function CheckoutProduct({id , image ,title ,price ,rating}) {
-  const [{basket}, dispatch ] = useStateValue();
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+  const [{ basket }, dispatch] = useStateValue()
   const removeFromBasket = () => {
     dispatch({
-      type: 'REMOVE_FROM_BASKET',
-      id: id, 
+      type: "REMOVE_FROM_BASKET",
+      id: id,
     })
   }
   return (
     <div className="checkoutProduct">
-      <img className="checkoutProduct__image"src={image}/>
+      <img className="checkoutProduct__image" src={image} />
 
       <div className="checkoutProduct__info">
-        <p className='checkoutProduct__title'>{title}</p>
+        <p className="checkoutProduct__title">{title}</p>
 
-        <p className='checkoutProduct__price'>
+        <p className="checkoutProduct__price">
           <small>$</small>
           <strong>{price}</strong>
         </p>
         <div className="checkoutProduct__rating">
-          {Array(rating).fill().map((_,i) => (<p>🌟</p>))}
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
-        <button onClick={removeFromBasket}>Remove From Basket</button>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove From Basket</button>
+        )}
       </div>
     </div>
   )
